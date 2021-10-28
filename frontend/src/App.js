5fef54998e5fff53e4ced876343309f89344a752
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-10-29 00:29:51 +0100

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import logo from './twitter.svg'
+
+const App = () => {
+    return (
+        <div className="App">
+            
+        </div>
+    )
+}
+
+export default App
