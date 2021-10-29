9479190f9bb645ca5d469554fc16bb10be7b0bff
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-10-29 06:15:47 +0100

    displaying trends as list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,49 @@
-import React from 'react'
-import logo from './twitter.svg'
-import { FaCrosshairs } from 'react-icons/fa'
-import './App.css'
+import React from "react";
+import logo from "./twitter.svg";
+import { FaCrosshairs } from "react-icons/fa";
+import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const App = () => {
+    const [trends, setTrends] = useState([]);
+    const [woeid, setWoeid] = useState(1);
+
+    useEffect(() => getTrends(), [woeid]);
+
+    function getTrends() {
+        axios
+            .get("/api/trends", {
+                params: {
+                    woeid,
+                },
+            })
+            .then((response) => {
+                // console.log(response.data);
+                setTrends(response.data[0].trends)
+            })
+            .catch((error) => console.log(error.message));
+    }
+
+    function handleLocation() {
+        alert("handle location");
+    }
+    function listTrends() {
+        return (
+            <ul>
+                {trends.map((trend, index) => (
+                    <li key={index}>
+                        <a href={trend.url}>{trend.name}</a>
+                        {trend.tweet_volume && (
+                            <span className="tweet-volume">
+                                {trend.tweet_volume}
+                            </span>
+                        )}
+                    </li>
+                ))}
+            </ul>
+        );
+    }
     return (
         <div className="App">
             <header className="App-header">
@@ -11,7 +51,10 @@ const App = () => {
                 <h3>Twitter Trends</h3>
             </header>
             <div className="menu">
-                <select name="trending-place">
+                <select
+                    name="trending-place"
+                    onChange={(e) => alert(e.target.value)}
+                >
                     <option value="1">Worldwide</option>
                     <option value="26734">Liverpool</option>
                     <option value="28218">Manchester</option>
@@ -31,12 +74,13 @@ const App = () => {
                     <option value="946738">Athens</option>
                     <option value="963291">Thessaloniki</option>
                 </select>
-                <div className="location">
+                <div className="location" onClick={handleLocation}>
                     <FaCrosshairs />
                 </div>
             </div>
+            <div className="content">{listTrends()}</div>
         </div>
     );
-}
+};
 
-export default App
+export default App;
